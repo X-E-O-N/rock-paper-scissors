b90fbd0f0dc5a09bf3971654130129a581696455
xeon <azmaintaraqqi@gmail.com>
2024-05-22 22:31:54 +0600

    add user input prompt, partial function for invalid input

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // we need a program that takes rock, paper or scissors as input and weighs it against a randomly generated output from those three.
 // if the output is the same as the input, that round is a draw. otherwise, the player or the computer gets a point depending on whose input beats that of the other.
 
-console.log("hlejo");
+let computerScore = 0;
+let userScore = 0;
+
+
 let getComputerChoice = function() {
     if (Math.random() <= 0.33) {
         return "rock";
@@ -11,3 +14,17 @@ let getComputerChoice = function() {
         return "scissors";
     }
 };
+
+let computerChoice = getComputerChoice();
+
+let getUserChoice = function() {
+    let choice = prompt(`what will you choose? \n (rock/paper/scissors)`).toLowerCase();   
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        return choice;
+    } else {
+        choice = prompt(`you didn't choose any of the given options! try again. \n (rock/paper/scissors)`).toLowerCase();
+    }
+};
+
+let userChoice = getUserChoice();
+
